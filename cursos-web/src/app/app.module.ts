567c02ece2d1fc74ca0cd6cf58d9224774d090e5
Fabricio Vallim <fmvallim@gmail.com>
2017-09-26 21:17:58 -0300

    Adicionando mecanismo de rota e menu

--- cursos-web/src/app/app.module.ts
+++ cursos-web/src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursoListagemComponent } from './cursos/curso-listagem.component';
+import { CursoCadastroComponent } from "./cursos/curso-cadastro.component";
 import { CursoService } from "./cursos/curso.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
+    CursoListagemComponent,
+    CursoCadastroComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'cursos', component: CursoListagemComponent },
+      { path: 'cursos/novo', component: CursoCadastroComponent },
+    ])
   ],
   providers: [CursoService],
   bootstrap: [AppComponent]
