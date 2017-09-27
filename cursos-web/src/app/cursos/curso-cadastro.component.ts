47dc2d27ed6e9b74ffaee61bac9dde16b4d943bb
Fabricio Vallim <fmvallim@gmail.com>
2017-09-26 21:20:14 -0300

    Adicionando esqueleto de componente de cadastro de curso

---
+++ cursos-web/src/app/cursos/curso-cadastro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./curso-cadastro.component.html",
+    styleUrls: ['./cursos.component.css']
+})
+export class CursoCadastroComponent {
+
+}
