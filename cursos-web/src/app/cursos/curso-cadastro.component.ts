5fe589b35074aa140c0c2455f52b1338869ed51d
Fabricio Vallim <fmvallim@gmail.com>
2017-09-28 21:45:20 -0300

    Criando tela de cadastro de curso

--- cursos-web/src/app/cursos/curso-cadastro.component.ts
+++ cursos-web/src/app/cursos/curso-cadastro.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Curso } from "./curso";
+import { CategoriaService } from "../shared/categoria.service";
+import { PeriodoService } from "../shared/periodo.service";
 
 @Component({
     templateUrl: "./curso-cadastro.component.html",
     styleUrls: ['./cursos.component.css']
 })
-export class CursoCadastroComponent {
+export class CursoCadastroComponent implements OnInit {
+    
+    private curso: Curso = new Curso();
 
+    private categorias: string[];
+
+    private periodos = []
+
+    constructor(private categoriaService: CategoriaService, private periodoService: PeriodoService) {}
+
+    ngOnInit(): void {
+        this.categoriaService.findAll().subscribe(data => this.categorias = data);
+        this.periodoService.findAll().subscribe(data => this.periodos = data);
+    }
+
+    salvar() {
+        console.log(this.curso);
+    }
 }
