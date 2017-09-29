15603085031a7ab0d047f49372358a66d7ded603
Fabricio Vallim <fmvallim@gmail.com>
2017-09-28 21:43:45 -0300

    Ajustando serviço para buscar cursos via api rest

--- cursos-web/src/app/cursos/curso.service.ts
+++ cursos-web/src/app/cursos/curso.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
-import { Curso } from "./curso";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { Configuration } from "../shared/configuration";
+import { Curso } from "./curso";
+
 import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class CursoService {
 
-    private endpoint = 'http://localhost:9000/api/cursos';
-
-    constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient, private configuration: Configuration) {}
 
     findAll(): Observable<Curso[]> {
-        return this.httpClient.get<Curso[]>(this.endpoint, {})
+        return this.httpClient.get<Curso[]>(this.configuration.getUrl('/cursos'), {})
     }
 }
